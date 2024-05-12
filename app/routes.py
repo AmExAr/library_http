12eb84a1b92a0d956a0b50929ef68a56fa4d5b28
username <mvaryushkin@mail.ru>
2024-05-12 15:01:59 +0500

    template index.html

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'amogus' : 'sussy baka'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['amogus'] + '''!</h1>
+    </body>
+</html>
+'''
